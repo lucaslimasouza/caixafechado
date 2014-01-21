58344f0e5316f320b76714978db4bb4ca6465458
Lucas Lima de Souza <lucaslima4p@gmail.com>
2014-01-21 20:58:22 -0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Caixafechado::Application.config.secret_key_base = '2e7af330e52b47d6595b94abf2f0cdb98d707ea30163142b887368225ab8721f7b98f78bd78e7afca4587ff2ebf0d37ee72f60f1885730b15648e613ff0a6580'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Caixafechado::Application.config.session_store :cookie_store, key: '_caixafechado_session'
