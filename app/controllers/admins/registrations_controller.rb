e810df5c63aeb80ce442fb7428598c6de075252b
Lucas Lima de Souza <lucaslima4p@gmail.com>
2014-01-23 00:06:11 -0200

    base of style sheets

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,5 +1,40 @@
 
-class Admins::RegistrionsController < Devise::RegistrationsController
+class Admins::RegistrationsController < Devise::RegistrationsController
 
+  def new
+    build_resource({})
+    self.resource.build_company
+    respond_with self.resource
+  end
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      resource.company
+      respond_with resource
+    end
+  end
+
+
+  def sign_up_params
+    params.
+      require(:admin).
+      permit(:email, :password, :password_confirmation,
+        company_attributes: [:address, :number, :cep, :phone, :name, :cnpj])
+  end
 
 end
