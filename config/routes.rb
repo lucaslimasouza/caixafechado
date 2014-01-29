33f177c5484f78d10f06222cd8b86ae59efe3e4c
Lucas Lima de Souza <lucaslima4p@gmail.com>
2014-01-28 23:05:40 -0200

    styling marketing message Home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Caixafechado::Application.routes.draw do
   get "home/index"
   devise_for :admins, controllers: {registrations: "admins/registrations"}
   resources :companies
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
