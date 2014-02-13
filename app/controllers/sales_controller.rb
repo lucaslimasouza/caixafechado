5c5dd6ce1e589b1ed4a084ef4f52727133209afe
Lucas Lima de Souza <lucaslima4p@gmail.com>
2014-02-12 22:04:42 -0200

    adding one product in Sale on session

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,9 @@
 class SalesController < ApplicationController
 
 	def add_products
+		@sale = Sale.new
+		@sale.products << Product.new(id: params[:id])
+		session[:sale] = @sale
 		redirect_to new_sale_path
 	end
 	
--- spec/controllers/sales_controller_spec.rb
+++ spec/controllers/sales_controller_spec.rb
@@ -7,9 +7,15 @@ describe SalesController do
 	  context '#add_products' do
 	  	it "should redirect to new template" do
 	  			get :add_products
+	  			
 	  			subject.should redirect_to new_sale_path
 	  	end
 
+			it "should have one product one session[:sale]" do
+				get :add_products, id: 1
+
+				expect(session[:sale].products.size).to be_equal 1				
+			end
 	  end
 	end
 
