361dac961c1a64f04c1cee042bb5c3dea6fe0c59
Lucas Lima de Souza <lucaslima4p@gmail.com>
2014-02-12 22:43:39 -0200

    refactoring all SaleController test

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,11 +1,17 @@
 class SalesController < ApplicationController
 
 	def add_products
-		admin_session[:sale].present? ? @sale = admin_session[:sale] : @sale = current_admin.company.sales.build
-		
-		@sale.products << Product.new(id: params[:id])
+		initialize_sale_on_session
+		@sale.products << Product.find(params[:id])
 		admin_session[:sale] = @sale
 		redirect_to new_sale_path
+	end
+
+	private
+	def initialize_sale_on_session
+		admin_session[:sale].present? ? 
+			@sale = admin_session[:sale] : 
+				@sale = current_admin.company.sales.build
 	end
 	
 end
--- spec/controllers/sales_controller_spec.rb
+++ spec/controllers/sales_controller_spec.rb
@@ -7,23 +7,28 @@ describe SalesController do
 	describe "GET" do
 	  
 	  context '#add_products' do
+
+			let(:product) { create(:product) }
+			let(:second_product) { create(:product) }
+			let(:sale_on_session) { subject.admin_session[:sale] }
+
 	  	it "should redirect to new template" do
-	  			get :add_products
+	  			get :add_products, id: product.id
 
 	  			subject.should redirect_to new_sale_path
 	  	end
 
 			it "should have one product on sale admin_session" do
-				get :add_products, id: 1
+				get :add_products, id: product.id
 
-				expect(subject.admin_session[:sale].products.size).to be_equal 1				
+				expect(sale_on_session.products.size).to be_equal 1				
 			end
 
 			it "should not overwrite some product added" do
-				get :add_products, id: 1
-				get :add_products, id: 2
+				get :add_products, id: product.id
+				get :add_products, id: second_product
 
-				expect(subject.admin_session[:sale].products.size).to be_equal 2 
+				expect(sale_on_session.products.size).to be_equal 2 
 			end
 	  end
 	end
