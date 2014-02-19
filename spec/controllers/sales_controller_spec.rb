63d909d937048e143691ff526a844d0fefdc9efd
Lucas Lima de Souza <lucaslima4p@gmail.com>
2014-02-18 21:59:40 -0300

    refactoring cancel product teste on Sale Controller and testing cancel product with 2 products on session

--- spec/controllers/sales_controller_spec.rb
+++ spec/controllers/sales_controller_spec.rb
@@ -7,10 +7,9 @@ describe SalesController do
 	describe "GET" do
 		let(:product) { create(:product) }
 		let(:sale_on_session) { subject.admin_session[:products_sale] }
+		let(:second_product) { create(:product) }
 	  
 	  context '#add_products' do
-
-			let(:second_product) { create(:product) }
 
 	  	it "should redirect to new template" do
 	  			get :add_products, id: product.id
@@ -36,7 +35,8 @@ describe SalesController do
 	  context '#cancel_products' do
 
 	  	it "should redirect to edit tamplete" do
-	  		allow(sale_on_session).to receive(:delete_if).and_return([])
+	  		allow_message_expectations_on_nil
+	  		allow(nil).to receive(:delete_if).and_return([])
 	  		
 	  		get :cancel_product, id: product.id
 
@@ -45,13 +45,19 @@ describe SalesController do
 
 	  	it "should have anyone product on sale admin" do
 	  		get :add_products, id: product.id
-
 	  		get :cancel_product, id: product.id
 
 	  		expect(sale_on_session.size).to be_equal 0
 	  	end
 
-	  	it "should have only one product on sale admin"
+	  	it "should have only one product on sale admin" do
+	  		get :add_products, id: product.id
+	  		get :add_products, id: second_product.id
+
+	  		get :cancel_product, id: product.id
+
+	  		expect(sale_on_session.size).to be_equal 1
+	  	end
 	  end
 
 	  context '#new' do
