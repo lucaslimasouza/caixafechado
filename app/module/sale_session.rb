80ed941b031e6de51b766f53d2e54df3b3591e9a
Lucas Lima de Souza <lucaslima4p@gmail.com>
2014-02-25 19:55:55 -0300

    refactoring SaleController#cancel_product

--- app/module/sale_session.rb
+++ app/module/sale_session.rb
@@ -1,8 +1,14 @@
 module SaleSession
+	
 	def add_products
 		initialize_products_sale_on_session
 		@products_sale << params[:id]
 		admin_session[:products_sale] = @products_sale
 		redirect_to new_sale_path
+	end
+
+	def cancel_product
+		admin_session[:products_sale].delete_if { |id| id == params[:id] }
+		redirect_to new_sale_path		
 	end
 end
